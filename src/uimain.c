11653116d95fe4482dcef3292570c7211c3ef08b
LorenzoMinjares22 <158229420+LorenzoMinjares22@users.noreply.github.com>
2024-10-22 08:29:25 -0600

    Update uimain.c

--- src/uimain.c
+++ src/uimain.c
@@ -1,85 +1,80 @@
 #include "stdio.h"
-
 #include "stdlib.h"
-
 #include "tokenizer.h"
-
 #include "history.h"
-
-
-
+ 
 int main(){
+  char input[100]; // Added a semicolon
 
-  char input[1000]; // Added a semicolon
   char *str;
+
   char **tokens;
+
   List *link_list = init_history();
+
   goto userInput;
 
-
-
+  
  userInput:
 
   printf("Input string input enter when done:");
-  _Bool keepGoing = 1;
-  while (keepGoing) {
+
+  while (1) {
 
     printf(">");
+
     fgets(input, sizeof(input), stdin);
 
-    if(input[0] == '\n') break;
 
+
+    if (input[0] == '\n')
+
+      break;
 
     str = input;
 
     tokens = tokenize(str);
 
-
-
     for (int i = 0; i < count_tokens(str); i++) {
-
-
 
       add_history(link_list, tokens[i]);
 
-
-
     }
-
-
 
     free(tokens); // Free the memory allocated for tokens
 
-
-
   }
 
-
-
   goto history;
-
-
-
-
 
 
 
  history:
 
   printf("Get all history: h |  Get specific history: ! | Done: d \n");
+
+
+
   char his;
+
   int id; // Changed id to integer type
+
+
 
   while (1) {
 
     his = getchar();
 
     if (his == '\n')
+
       continue;
 
     if (his == 'h') {
+
       print_history(link_list);
+
       printf("Get all history: h |  Get specific history: ! | Done: d \n");
+
     } else if (his == '!') {
 
       printf("input ID of history: ");
@@ -110,6 +105,3 @@ int main(){
   return 0;
 
 }
-
-
-
